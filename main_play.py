99706e621a515965062ef1bd5c0d67fadb81b0e8
jssosa10 <js.sosa10@uniandes.edu.co>
2019-09-23 21:53:29 -0500

    play

---
+++ main_play.py
@@ -0,0 +1,24 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+from utils.downsample_wrapper import wrap_deepmind
+from utils.schedule import LinearSchedule
+from gym import wrappers
+from networks.DQN import DQN
+
+import torch
+import numpy as np
+import random
+
+SEED = 1
+
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+env.seed(SEED)
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
+
+env = wrap_deepmind(env)
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
+expt_dir = 'Game_play'
+env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda episode_id: True)
---
+++ scripts/play.py
@@ -0,0 +1,49 @@
+import os
+from itertools import count
+
+import torch
+import torch.autograd as autograd
+
+
+USE_CUDA = torch.cuda.is_available()
+print(USE_CUDA)
+dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+
+class Variable(autograd.Variable):
+    def __init__(self, data, *args, **kwargs):
+        if USE_CUDA:
+            data = data.cuda()
+        super(Variable, self).__init__(data, *args, **kwargs)
+
+def dqn_play(
+	env,
+	q_func):
+
+    img_h, img_w, img_c = env.observation_space.shape
+    input_arg = img_c
+    num_actions = env.action_space.n
+
+    def load_model(model):
+        if os.path.isfile('mario_Q_params.pkl'):
+            print('Load Q parametets ...')
+            model.load_state_dict(torch.load('mario_Q_params.pkl'))
+        return model
+
+    def get_action(model, obs):
+        obs = torch.from_numpy(obs).type(dtype).unsqueeze(0)/255.0
+        return model(Variable(obs)).data.max(1)[1].view(-1, 1).cpu()
+
+    Q = q_func(input_arg, num_actions).type(dtype)
+    Q = load_model(Q)
+
+    obs = env.reset()
+
+    for t in count():
+
+        action = get_action(Q, obs.transpose(2, 0, 1))
+
+        obs, reward, done, _ = env.step(action)
+
+        if done:
+            obs = env.reset()

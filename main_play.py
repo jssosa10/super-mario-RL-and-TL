dba5518857991b83bfd43360f76c452a6006bdb3
jssosa10 <js.sosa10@uniandes.edu.co>
2019-09-23 21:55:56 -0500

    play

--- main_play.py
+++ main_play.py
@@ -2,9 +2,10 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 from utils.downsample_wrapper import wrap_deepmind
-from utils.schedule import LinearSchedule
 from gym import wrappers
 from networks.DQN import DQN
+from scripts.play import dqn_play
+
 
 import torch
 import numpy as np
@@ -22,3 +23,7 @@ env = wrap_deepmind(env)
 env = JoypadSpace(env, COMPLEX_MOVEMENT)
 expt_dir = 'Game_play'
 env = wrappers.Monitor(env, expt_dir, force=True, video_callable=lambda episode_id: True)
+dqn_play(
+    env=env,
+    q_func=DQN,
+)
